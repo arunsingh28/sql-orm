90f948e60e2582e00b15617dc73cd0f47ebbef31
Arun Singh <arunsingh28aug.as@gmail.com>
2021-12-15 13:33:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const colleciton = process.env.COLLECTION || 'collection_name'
+
 const sql = {
     findUser : (database)=>{
-        return `SELECT * FROM ${database.email}`
+        return `SELECT ${database[Object.keys(database)[0]]} FROM ${colleciton}`
     }
 }
 
